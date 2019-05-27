9438b2920c3b4b3113b95a6fffd43e15d952b7ab
Danial Nickford <admin@redsparr0w.com>
2019-05-27 14:26:50 +1200

    start with 0 prestige points

--- src/scripts/Player.ts
+++ src/scripts/Player.ts
@@ -10,7 +10,7 @@ class Player {
 
     public achievementsCompleted: { [name: string]: boolean };
     public prestigesCompleted: number[] = [0,0,0];
-    public prestigePoints: number[] = [20,20,20];
+    public prestigePoints: number[] = [0,0,0];
     public prestigeUpgradesBought = new Array(GameConstants.AMOUNT_OF_PRESTIGE_UPGRADES).fill(false);
 
     private _caughtShinyList: KnockoutObservableArray<string>;
@@ -1015,4 +1015,3 @@ class Player {
     }
 
 }
-
